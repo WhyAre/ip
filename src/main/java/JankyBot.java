0451cf6edaa79b0205de9256ddbb8d471f30c218
WhyAre <laiyr52@gmail.com>
2025-01-16 15:50:44 +0800

    Add feature to mark as done

--- src/main/java/JankyBot.java
+++ src/main/java/JankyBot.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 
 public class JankyBot {
     private static final String name = "JankyBot";
-    private static final ArrayList<String> memory = new ArrayList<>();
+    private static final ArrayList<Task> memory = new ArrayList<>();
 
     static void greet() {
         System.out.printf("Hello! I'm %s\nWhat can I do for you?\n", name);
@@ -16,7 +16,13 @@ public class JankyBot {
     }
 
     static void addToList(String line) {
-        memory.add(line);
+        memory.add(new Task(line));
+    }
+
+    static Task markListItem(int index, boolean isMarked) {
+        var newTask = memory.get(index).setMark(isMarked);
+        memory.set(index, newTask);
+        return newTask;
     }
 
     static void printList() {
@@ -39,8 +45,22 @@ public class JankyBot {
                     switch (input) {
                         case "list" -> printList();
                         default -> {
-                            addToList(input);
-                            System.out.printf("added: %s\n", input);
+                            if (input.startsWith("mark")) {
+                                int index = Integer.parseInt(input.substring("mark ".length())) - 1;
+                                var task = markListItem(index, true);
+                                System.out.printf("Nice! I've marked this task as done:\n%s\n",
+                                        task);
+                            } else if (input.startsWith("unmark")) {
+                                int index =
+                                        Integer.parseInt(input.substring("unmark ".length())) - 1;
+                                var task = markListItem(index, false);
+                                System.out.printf(
+                                        "Nice! I've marked this task as not done yet:\n%s\n",
+                                        task);
+                            } else {
+                                addToList(input);
+                                System.out.printf("added: %s\n", input);
+                            }
                         }
                     }
                 });
@@ -48,5 +68,24 @@ public class JankyBot {
         sc.close();
 
         bye();
+    }
+
+    record Task(String title, boolean isMarked) {
+        Task(String title) {
+            this(title, false);
+        }
+
+        Task setMark(boolean isMarked) {
+            return new Task(this.title, isMarked);
+        }
+
+        private char getStatusIcon() {
+            return (this.isMarked ? 'X' : ' ');
+        }
+
+        @Override
+        public String toString() {
+            return "[%s] %s".formatted(this.getStatusIcon(), this.title);
+        }
     }
 }
