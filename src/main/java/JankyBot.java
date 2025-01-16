8a0c2d704713da142379c0f30d630ba1bd181cc9
WhyAre <laiyr52@gmail.com>
2025-01-16 15:10:05 +0800

    Extract greet and bye into functions

--- src/main/java/JankyBot.java
+++ src/main/java/JankyBot.java
@@ -1,9 +1,17 @@
 public class JankyBot {
     private static final String name = "JankyBot";
 
-    public static void main(String[] args) {
+    static void greet() {
         System.out.printf("Hello! I'm %s\nWhat can I do for you?\n", name);
+    }
 
+    static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+
+        bye();
     }
 }
