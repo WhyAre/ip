e9ab61e89f1b4e80164c0551de90f83df5d00647
WhyAre <laiyr52@gmail.com>
2025-01-16 15:09:10 +0800

    Add feature to greet user and exit

--- src/main/java/JankyBot.java
+++ src/main/java/JankyBot.java
@@ -1,10 +1,9 @@
 public class JankyBot {
+    private static final String name = "JankyBot";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.printf("Hello! I'm %s\nWhat can I do for you?\n", name);
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
