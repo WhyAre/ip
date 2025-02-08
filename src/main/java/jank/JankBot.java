9a9dd42db8cbf640db18b5fec14cc302c3829663
WhyAre <laiyr52@gmail.com>
2025-02-08 21:51:44 +0800

    Convert static methods in JankBot to non-static

--- src/main/java/jank/JankBot.java
+++ src/main/java/jank/JankBot.java
@@ -6,24 +6,19 @@ import java.util.stream.Collectors;
  * Main Bot
  */
 public class JankBot {
-    private static final String TASK_FILE = "./data/jank.txt";
     private static final String NAME = "JankBot";
 
-    private static final TaskList tasks = Storage.loadTasks(TASK_FILE);
+    private final TaskList tasks;
+    private final String taskFile;
 
     /**
-     * Prints greet message
+     * Constructs a new JankBot object
+     *
+     * @param taskFile file path to store tasks
      */
-    static String greet() {
-        return """
-                     _   _    _   _ _  __
-                    | | / \\  | \\ | | |/ /
-                 _  | |/ _ \\ |  \\| | ' /
-                | |_| / ___ \\| |\\  | . \\
-                 \\___/_/   \\_\\_| \\_|_|\\_\\
-                Hello! I'm %s
-                What can I do for you?
-                """.formatted(NAME);
+    public JankBot(String taskFile) {
+        this.taskFile = taskFile;
+        tasks = Storage.loadTasks(taskFile);
     }
 
     /**
@@ -60,6 +55,20 @@ public class JankBot {
 //        System.out.printf("Now you have %d tasks in the list.\n", tasks.size());
     }
 
+    /**
+     * Prints greet message
+     */
+    String greet() {
+        return """
+                     _   _    _   _ _  __
+                    | | / \\  | \\ | | |/ /
+                 _  | |/ _ \\ |  \\| | ' /
+                | |_| / ___ \\| |\\  | . \\
+                 \\___/_/   \\_\\_| \\_|_|\\_\\
+                Hello! I'm %s
+                What can I do for you?
+                """.formatted(NAME);
+    }
 
     /**
      * Performs the action that's supplied into the function
@@ -67,7 +76,7 @@ public class JankBot {
      * @param line command as a String[]
      * @throws JankBotException
      */
-    static String executeCommand(String[] line) throws JankBotException {
+    String executeCommand(String[] line) throws JankBotException {
         String cmd = line[0];
 
         return switch (cmd) {
@@ -88,7 +97,7 @@ public class JankBot {
             case "delete" -> {
                 var c = DeleteCommand.parse(line);
                 var deletedTask = tasks.remove(c.index());
-                Storage.saveTasks(TASK_FILE, tasks);
+                Storage.saveTasks(taskFile, tasks);
                 yield getDelSuccessMsg(deletedTask);
             }
             case "mark", "unmark" -> {
@@ -98,26 +107,26 @@ public class JankBot {
                         ? "Nice! I've marked this task as done:\n%s\n".formatted(tasks.mark(c.index()))
                         : "Nice! I've marked this task as not done yet:\n%s\n".formatted(tasks.unmark(c.index()));
 
-                Storage.saveTasks(TASK_FILE, tasks);
+                Storage.saveTasks(taskFile, tasks);
 
                 yield output;
             }
             case "todo" -> {
                 var c = TodoCommand.parse(line);
                 var newTask = tasks.add(new TodoTask(c.desc()));
-                Storage.saveTasks(TASK_FILE, tasks);
+                Storage.saveTasks(taskFile, tasks);
                 yield getAddSuccessMsg(newTask);
             }
             case "deadline" -> {
                 var c = DeadlineCommand.parse(line);
                 var newTask = tasks.add(new DeadlineTask(c.desc(), c.by()));
-                Storage.saveTasks(TASK_FILE, tasks);
+                Storage.saveTasks(taskFile, tasks);
                 yield getAddSuccessMsg(newTask);
             }
             case "event" -> {
                 var c = EventCommand.parse(line);
                 var newTask = tasks.add(new EventTask(c.desc(), c.from(), c.to()));
-                Storage.saveTasks(TASK_FILE, tasks);
+                Storage.saveTasks(taskFile, tasks);
                 yield getAddSuccessMsg(newTask);
             }
             default -> throw new JankBotException("I don't know what that means");
